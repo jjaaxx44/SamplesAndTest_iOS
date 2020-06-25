b2bf462f986233d427d79bf20e5b4a9fb8c06723
Abhishek <abhishek.chaudhari44@gmail.com>
2020-06-25 23:08:53 +0530

    added sample code

--- ProtocolTest/ProtocolTest/ViewController.swift
+++ ProtocolTest/ProtocolTest/ViewController.swift
@@ -7,14 +7,49 @@
 //
 
 import UIKit
+protocol TestProtocol {
+    var testString: String { get }
+}
+
+class TestClass1: TestProtocol {
+    var testString: String
+    init(testString: String) {
+        self.testString = testString
+    }
+}
+
+class TestClass2: TestProtocol {
+    var testString: String
+    init(testString: String) {
+        self.testString = testString
+    }
+}
 
 class ViewController: UIViewController {
 
+    var testArray: [TestProtocol] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let testObj1 = TestClass1(testString: "TestClass1 obj")
+        
+        let testObj2: TestProtocol = TestClass1(testString: "TestClass1 confirming protocol")
+        
+        let testObj3: TestClass2 = TestClass2(testString: "TestClass2 confirming protocol")
+
+        testArray.append(testObj1)
+        testArray.append(testObj2)
+        testType(object: testObj3)
+
+        for obj in testArray {
+            print(obj.testString)
+        }
     }
-
-
+    
+    func testType<T:TestProtocol>(object: T) {
+        testArray.append(object)
+    }
 }
 
